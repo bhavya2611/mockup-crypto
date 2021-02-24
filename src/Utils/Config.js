64aadb280f72f1dd2c00cbcbfcc2589ffdb009fb
bhavya2611 <bhavya.y.mehta@gmail.com>
2021-02-24 17:57:24 +0530

    css fix to align content on top

--- src/Utils/Config.js
+++ src/Utils/Config.js
@@ -1,43 +1,47 @@
-export const CONFIGS = [{
+export const CONFIGS = [
+  {
     name: "Nora M. Buchanan",
     description: "4 interactions",
     email: "invision@invisionapp.com",
     phone: "+144-3412-4422",
     location: "New York, NY",
     metrics: [
-        { label: "Distance", value: "12.3 km" },
-        { label: "Time", value: "42 min" },
-        { label: "Price", value: "$34.20" },
-        { label: "Energy", value: "12.4 kWh" }
+      { label: "Distance", value: "12.3 km" },
+      { label: "Time", value: "42 min" },
+      { label: "Price", value: "$34.20" },
+      { label: "Energy", value: "12.4 kWh" },
     ],
     address_1: { line1: "Jackson Heights", line2: "37-27 74th Street" },
     address_2: { line1: "Greenpoint", line2: "81 Gate StBrooklyn" },
-}, {
+  },
+  {
     name: "Amy Santiago",
     description: "6 interactions",
     email: "santiagoamy@invisionapp.com",
     phone: "+144-3412-1234",
     location: "New York, NY",
     metrics: [
-        { label: "Distance", value: "12 km" },
-        { label: "Time", value: "22 min" },
-        { label: "Price", value: "$20.20" },
-        { label: "Energy", value: "11.5 kWh" }
+      { label: "Distance", value: "12 km" },
+      { label: "Time", value: "22 min" },
+      { label: "Price", value: "$20.20" },
+      { label: "Energy", value: "11.5 kWh" },
     ],
     address_1: { line1: "Old Forge", line2: "2305  Oak Street" },
     address_2: { line1: "Leminwood Ltd", line2: "81 Gate StBrooklyn" },
-}, {
+  },
+  {
     name: "Luke Skywalker",
     description: "7 interactions",
     email: "lukesky@invisionapp.com",
     phone: "+144-1234-9999",
     location: "New York, NY",
     metrics: [
-        { label: "Distance", value: "30.3 km" },
-        { label: "Time", value: "42 min" },
-        { label: "Price", value: "$50.20" },
-        { label: "Energy", value: "34.4 kWh" }
+      { label: "Distance", value: "30.3 km" },
+      { label: "Time", value: "42 min" },
+      { label: "Price", value: "$50.20" },
+      { label: "Energy", value: "34.4 kWh" },
     ],
-    address_1: { line1: "The Banqueting House", line2: "37-27 74th Street" },
+    address_1: { line1: "The Banq House", line2: "37-27 74th Street" },
     address_2: { line1: "Starline Cabs", line2: "81 Gate StBrooklyn" },
-}];
+  },
+];
